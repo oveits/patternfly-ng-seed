a50d454bfed28880487082c8206b02ed1875396a
Mike Echevarria <mechevar@redhat.com>
2018-05-24 16:06:56 +0000

    add donut compoment logic

--- src/app/donut/donut.component.ts
+++ src/app/donut/donut.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardConfig, DonutConfig } from 'patternfly-ng'
 
 @Component({
   selector: 'app-donut',
@@ -7,6 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class DonutComponent implements OnInit {
 
   constructor() { }
+  
+  cardConfig: CardConfig = {
+    title: 'Donut View',
+    noPadding: false
+  };
+
+  data: any[] = [
+    ['PC', 13],
+    ['Tablet', 6],
+    ['Phone', 22]
+  ];
+
+  donutConfig: DonutConfig = {
+    chartId: 'donutChart',
+    colors: {
+      PC: '#0088ce', // blue
+      Tablet: '#3f9c35', // green
+      Phone: '#cc0000'// red
+    },
+    donut: {
+      title: 'Inventory'
+    },
+    legend: {
+      show: true
+    }
+  };
 
   ngOnInit() {
   }
